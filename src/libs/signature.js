34ff41a5e2419dfdffa0287bc702784d821513ae
Fernando Rodríguez Sela <fernando@ferimer.es>
2024-06-06 11:45:22 +0200

    NIT: s/signatureDate/signatureData/g

--- src/libs/signature.js
+++ src/libs/signature.js
@@ -8,14 +8,14 @@ import bs58 from 'bs58'
  * Validate Solana Web3 signature (with Phantom)
  *
  * @param message - Signed message as String
- * @param signatureDate - Signature object with signature serialized buffer and Solana publicKey
+ * @param signatureData - Signature object with signature serialized buffer and Solana publicKey
  */
-export function validate(message, signatureDate) {
+export function validate(message, signatureData) {
   try {
     return nacl.sign.detached.verify(
       naclUtil.decodeUTF8(message),
-      Buffer.from(signatureDate.signature),
-      bs58.decode(signatureDate.publicKey),
+      Buffer.from(signatureData.signature),
+      bs58.decode(signatureData.publicKey),
     )
   } catch (e) {
     return false
