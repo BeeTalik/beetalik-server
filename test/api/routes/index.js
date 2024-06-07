9233632bfe5f59c480b7f5f79b312381e25d584f
Fernando Rodríguez Sela <fernando@ferimer.es>
2024-06-07 10:43:05 +0200

    Added GET login challenge endpoint

--- test/api/routes/index.js
+++ test/api/routes/index.js
@@ -2,8 +2,10 @@
 
 import root from './root.js'
 import beetalikV1 from './beetalik/v1/index.js'
+import loginChallengeTests from './beetalik/v1/login/challenge/index.js'
 
 export default () => {
   describe('Root routes', root)
   describe('beetalik/v1 routes', beetalikV1)
+  describe('beetalik/v1/login/challenge tests', loginChallengeTests)
 }
