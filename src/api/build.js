a12b9269b1db7418bff56da96575be3382fd480e
Fernando Rodríguez Sela <fernando@ferimer.es>
2024-06-07 09:40:04 +0200

    Enabled fastify-overview and fastify-overview-ui on development mode

--- src/api/build.js
+++ src/api/build.js
@@ -9,6 +9,21 @@ import { dirname, join } from 'path'
 export async function build(opts) {
   const fastify = Fastify(opts)
 
+  if (process.env.NODE_ENV === 'development') {
+    await fastify.register(await import('fastify-overview'), {
+      exposeRoute: true,
+    })
+    await fastify.register(await import('fastify-overview-ui'))
+    fastify.addHook('onReady', () => {
+      fastify.overview()
+    })
+    fastify.addHook('onRequest', (req, reply, done) => {
+      req.devMode = true
+      req.overviewRoutes = ['json-overview', 'fastify-overview-ui/']
+      done()
+    })
+  }
+
   const __filename = fileURLToPath(import.meta.url)
   const __dirname = dirname(__filename)
   fastify.register(autoLoad, {
--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -4,6 +4,19 @@ import config from 'config'
 
 export default async function MainRoute(app) {
   app.get('/', async function (request, reply) {
-    reply.send(`Welcome to ${config.info.name} version ${config.info.version}`)
+    let devExtra = ''
+    if (request.devMode === true) {
+      devExtra = `DEVELOPMENT MODE ENABLED: ${request.overviewRoutes.map(x => `<a href='${x}'>${x}</a>`).join(', ')})`
+    }
+    reply.header('content-type', 'text/html').send(`<!DOCTYPE html>
+<html>
+<head>
+<title>${config.info.name} version ${config.info.version}</title>
+</head>
+<body>
+<h1>Welcome to ${config.info.name} version ${config.info.version}</h1>
+<p>${devExtra}</p>
+</body>
+</html>`)
   })
 }
